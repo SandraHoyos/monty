df38bd801a2c62286aecdbd2491bf8f14fe4d6eb
SandraHoyos <slhoyosr@gmail.com>
2021-09-22 22:38:59 -0500

    adding new functions

--- monty.h
+++ monty.h
@@ -1,11 +1,33 @@
-/*
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct var
+{
+	int holder;
+	char check;
+}var_t;
+
+extern var_t variables;
+var_t variables;
+
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -13,13 +35,14 @@ typedef struct stack_s
         struct stack_s *prev;
         struct stack_s *next;
 } stack_t;
+
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -38,6 +61,14 @@ void pop(stack_t **stack, unsigned int linenum);
 void swap(stack_t **stack, unsigned int linenum);
 void add(stack_t **stack, unsigned int linenum);
 void nop(stack_t **stack, unsigned int linenum);
-
+void sub(stack_t **stack, unsigned int linenum);
+void _div(stack_t **stack, unsigned int linenum);
+void mul(stack_t **stack, unsigned int linenum);
+void mod(stack_t **stack, unsigned int linenum);
+void pchar(stack_t **stack, unsigned int linenum);
+void pstr(stack_t **stack, unsigned int linenum);
+void rotl(stack_t **stack, unsigned int linenum);
+void rotr(stack_t **stack, unsigned int linenum);
+void _queue(stack_t **stack, unsigned int linenum);
 
 #endif
